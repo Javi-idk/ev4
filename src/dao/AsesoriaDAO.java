17551bd3c1f11f54def2cca2167463e93c9fd538
Javi-idk <javiera.soto.ar@gmail.com>
2020-07-04 01:17:00 -0400

    commit1

---
+++ src/dao/AsesoriaDAO.java
@@ -0,0 +1,25 @@
+package dao;
+
+import java.util.List;
+
+public class AsesoriaDAO implements ICR {
+
+	@Override
+	public boolean guardar(Object o) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public List<Object> listarTodo() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Object buscarPorID(int id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
